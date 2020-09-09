c04b6ee49064dc5cdd07eef81c8336730ebb7479
Jongdee H <jannykosin@gmail.com>
2020-09-09 15:25:11 -0700

    test

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
 //  * This function "hasName" returns whether the person object has a non-empty name.
 //  * If the name property of person is null or is an empty string, returns false. If not returns true.
 function hasName(person) {
-  return person.name != null && person.name.length > 0
+  return person.name != null && person.name.length > 0;
 }
 
 // ---------------------------------------------------------------- //
@@ -23,7 +23,7 @@ function hasName(person) {
 // STEP 2:
 // Explain line by line what the function does.
 function hasName(person) {
-  return person.name != null && person.name.length > 0
+  return person.name != null && person.name.length > 0;
   // First, make sure that the name property is not null. If it is null, immediately return false.
   // If it is not null, check the length of name. If there is at least one character in name, return
   // true, otherwise return false.
@@ -31,7 +31,8 @@ function hasName(person) {
 
 // EXERCISE 1
 function greet(name) {
-  return "Hello " + name + "!"
+  return "Hello " + name + "!";
 }
 
-console.log(greet("Eric"))
+console.log(greet("Eric"));
+// hi teya
